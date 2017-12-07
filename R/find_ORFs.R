89804d85649039f50079e3eafdda25097e7eca7a
Roleren <hauken_heyken@hotmail.com>
2017-12-07 17:47:28 +0100

    first usable commit of find_orfs_fa

--- R/find_ORFs.R
+++ R/find_ORFs.R
@@ -98,11 +98,11 @@ find_in_frame_ORFs <- function(grl, fastaSeqs, startCodon =  "ATG",
   if(length(fastaSeqs) == 0) stop("Fasta sequences had length 0")
 
   result <- ORFs_as_List(fastaSeqs = as.matrix(as.character( fastaSeqs)),
-                                 startCodon = startCodon,stopCodon = stopCodon,
+                                 startCodon = startCodon, stopCodon = stopCodon,
                                  longestORF = longestORF,
                                  minimumLength = minimumLength)
 
-  return(map_to_GRanges(grl, result) )
+  return(map_to_GRanges(grl, result))
 }
 
 
@@ -121,267 +121,16 @@ find_in_frame_ORFs <- function(grl, fastaSeqs, startCodon =  "ATG",
 #' @return A List of GRanges of ORFs mapped to fasta file. Each ORF includes START and STOP codon,
 #' frame, seqname, and strand.
 #' @export
-#' @import S4Vectors
-#' @import seqinr
-#' @import IRanges
 #' @examples
 #' #find_ORFs_fa()
 #'
-find_ORFs_fa <-
-  function(file,
-           startCodon = c("ATG"),
-           stopCodon = c("TAA|TAG|TGA"),
+find_ORFs_fa <- function(file, startCodon = "ATG",
+           stopCodon = "TAA|TAG|TGA",
            longestORF = T,
            minimumLength = 0) {
-    message("Loading fasta file.")
-    refseq <- read.fasta(file)
-    message("Preparing reverse complementary sequence.")
-    refseq_r <- lapply(refseq, function(x)
-      rev(comp(x)))
-    message("Defining frames and stop codons.")
-    f0 <-
-      getTrans(
-        refseq,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 0,
-        numcode = 1
-      )
-    f1 <-
-      getTrans(
-        refseq,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 1,
-        numcode = 1
-      )
-    f2 <-
-      getTrans(
-        refseq,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 2,
-        numcode = 1
-      )
-    f0r <-
-      getTrans(
-        refseq_r,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 0,
-        numcode = 1
-      )
-    f1r <-
-      getTrans(
-        refseq_r,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 1,
-        numcode = 1
-      )
-    f2r <-
-      getTrans(
-        refseq_r,
-        sens = "F",
-        NAstring = "N",
-        ambiguous = FALSE,
-        frame = 2,
-        numcode = 1
-      )
+  if (class(file) != "character") stop("file must be type character")
+  if(!file.exists(file)) stop("file does not exist, check working dir!")
 
-    stopCodons <- function(x) {
-      return(which(x == "*"))
-    }
-
-    f0 <- lapply(f0, stopCodons)
-    f0 <- lapply(f0, function(x)
-      x * 3)
-    f1 <- lapply(f1, stopCodons)
-    f1 <- lapply(f1, function(x)
-      x * 3 + 1)
-    f2 <- lapply(f2, stopCodons)
-    f2 <- lapply(f2, function(x)
-      x * 3 + 2)
-    f0r <- lapply(f0r, stopCodons)
-    f0r <- lapply(f0r, function(x)
-      x * 3)
-    f1r <- lapply(f1r, stopCodons)
-    f1r <- lapply(f1r, function(x)
-      x * 3 + 1)
-    f2r <- lapply(f2r, stopCodons)
-    f2r <- lapply(f2r, function(x)
-      x * 3 + 2)
-
-    seq_names <- names(refseq)
-    finalRanges <- GRanges()
-
-    for (seqname in 1:length(seq_names)) {
-      message(paste0("Finding ORFs for "), seq_names[seqname])
-      # plus strand f0
-      f0ORFs <- c()
-      for (chunk in 1:length(f0[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f0[[seqname]][chunk - 1] + 1)
-        endo <- f0[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <- shift(tempORF, starto - 1)
-        f0ORFs <-
-          c(tempORF[end(tempORF) == endo], f0ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f0ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f0ORFs))), f0ORFs, Rle(strand(rep(
-          "+", length(f0ORFs)
-        ))))
-      f0ORFs$Frame <- rep("0", length(f0ORFs))
-      finalRanges <- c(f0ORFs, finalRanges)
-
-      # f1
-      f1ORFs <- c()
-      for (chunk in 1:length(f1[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f1[[seqname]][chunk - 1] + 1)
-        endo <- f1[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <- shift(tempORF, starto - 1)
-        f1ORFs <-
-          c(tempORF[end(tempORF) == endo], f1ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f1ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f1ORFs))), f1ORFs, Rle(strand(rep(
-          "+", length(f1ORFs)
-        ))))
-      f1ORFs$Frame <- rep("1", length(f1ORFs))
-      finalRanges <- c(f1ORFs, finalRanges)
-
-      # f2
-      f2ORFs <- c()
-      for (chunk in 1:length(f2[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f2[[seqname]][chunk - 1] + 1)
-        endo <- f2[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <- shift(tempORF, starto - 1)
-        f2ORFs <-
-          c(tempORF[end(tempORF) == endo], f2ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f2ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f2ORFs))), f2ORFs, Rle(strand(rep(
-          "+", length(f2ORFs)
-        ))))
-      f2ORFs$Frame <- rep("2", length(f2ORFs))
-      finalRanges <- c(f2ORFs, finalRanges)
-
-      # minus strand
-      full_string <- length(refseq_r[[seqname]])
-      # f0
-      f0ORFs <- c()
-      for (chunk in 1:length(f0r[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f0r[[seqname]][chunk - 1] + 1)
-        endo <- f0r[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq_r[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <- shift(tempORF, starto - 1)
-        f0ORFs <-
-          c(tempORF[end(tempORF) == endo], f0ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f0ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f0ORFs))),
-                IRanges(full_string - end(f0ORFs) + 1, width = width(f0ORFs)),
-                Rle(strand(rep(
-                  "-", length(f0ORFs)
-                ))))
-      f0ORFs$Frame <- rep("0", length(f0ORFs))
-      finalRanges <- c(f0ORFs, finalRanges)
-
-      # f1
-      f1ORFs <- c()
-      for (chunk in 1:length(f1r[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f1r[[seqname]][chunk - 1] + 1)
-        endo <- f1r[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq_r[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <- shift(tempORF, starto - 1)
-        f1ORFs <-
-          c(tempORF[end(tempORF) == endo], f1ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f1ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f1ORFs))),
-                IRanges(full_string - end(f1ORFs) + 1, width = width(f1ORFs)),
-                Rle(strand(rep(
-                  "-", length(f1ORFs)
-                ))))
-      f1ORFs$Frame <- rep("1", length(f1ORFs))
-      finalRanges <- c(f1ORFs, finalRanges)
-
-      # f2
-      f2ORFs <- c()
-      for (chunk in 1:length(f2r[[seqname]])) {
-        starto <- ifelse(chunk == 1, 1, f2r[[seqname]][chunk - 1] + 1)
-        endo <- f2r[[seqname]][chunk]
-        seq_to_search <-
-          toupper(getSequence(refseq_r[[seqname]][starto:endo], as.string = T))
-        tempORF <-
-          find_in_frame_ORFs(
-            seq_to_search,
-            startCodon = startCodon,
-            longestORF = longestORF,
-            minimumLength = minimumLength
-          )
-        tempORF <-
-          shift(tempORF, starto - 1)
-        f2ORFs <-
-          c(tempORF[end(tempORF) == endo], f2ORFs)  #stop codon must be same as in ORF so we know frame is correct
-      }
-      f2ORFs <-
-        GRanges(Rle(rep(seq_names[seqname], length(f2ORFs))),
-                IRanges(full_string - end(f2ORFs) + 1, width = width(f2ORFs)),
-                Rle(strand(rep(
-                  "-", length(f2ORFs)
-                ))))
-      f2ORFs$Frame <-
-        rep("2", length(f2ORFs))
-      finalRanges <- c(f2ORFs, finalRanges)
-    }
-
-    finalRanges <- sort(finalRanges)
-  }
+  ORFs <- find_ORFs_faC(file,startCodon,stopCodon,longestORF,minimumLength)
+  return(ORFs)
+}
---
+++ src/find_ORFs_fa.cpp
@@ -0,0 +1,148 @@
+// Find many orfs faster by putting all the data into Cpp directly
+// Cpp format: Webkit: 80 character max line
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cassert>
+
+#include <Rcpp.h>
+#include "findORFsHelpers.h"
+
+using vi = std::vector<int>;
+using namespace Rcpp;
+
+Function GRangesC("GRanges", Environment::namespace_env("GenomicRanges"));
+Function IRangesC("IRanges", Environment::namespace_env("IRanges"));
+
+
+char complement(char n)
+{
+  switch(n)
+  {
+  case 'A':
+    return 'T';
+  case 'T':
+    return 'A';
+  case 'G':
+    return 'C';
+  case 'C':
+    return 'G';
+  case 'N':
+    return 'N';
+  case 'a':
+    return 't';
+  case 't':
+    return 'a';
+  case 'g':
+    return 'c';
+  case 'c':
+    return 'g';
+  }
+  assert(false);
+  return ' ';
+}
+
+// ! Only for procaryotic circular genomes
+// Takes a fasta file and finds all orfs,
+// including orfs that span from before stop to over start position of genome
+// [[Rcpp::export]]
+S4 findORFs_procaryote(std::string file,
+                 std::string startCodon,
+                 std::string stopCodon,
+                 bool longestORF,
+                 int minimumLength){
+  std::vector<int> all_orfs;
+  std::vector<int> Seqnames;
+  std::vector<std::string> strands;
+  std::ifstream in(file.c_str());
+  in.get(); // remove first '>'
+  std::string rec;
+  int n = 0;
+  while(getline(in,rec,'>')){ // For each chromosome
+    int newLineLoc = rec.find('\n');
+    std::string header = rec.substr(0,newLineLoc);
+    std::string fastaSeq = rec.substr(newLineLoc+1, rec.length()-newLineLoc-2);
+  std::vector<int> ORFdef = orfs_as_vector(fastaSeq, startCodon,
+                                             stopCodon,longestORF,
+                                             minimumLength); // <- get all orfs for start to stop
+    all_orfs.insert(all_orfs.end(), ORFdef.begin(), ORFdef.end());
+    Seqnames.insert(Seqnames.end(), ORFdef.size() / 2, n++);
+    strands.insert(strands.end(),ORFdef.size() / 2,"+");
+
+    //Now do stop/start boundary, +/-3000, only keep the ones who are overlapping start/stop boundary
+    std::string startStopBoundary = fastaSeq.substr(fastaSeq.length()-3000,3000);
+    startStopBoundary.append(fastaSeq.substr(0,3000));
+    std::vector<int> ORFdefBoundary = orfs_as_vector(startStopBoundary, startCodon,
+                                             stopCodon,longestORF,
+                                             minimumLength);
+    //now filter out wrong ones, the ones that does not contain point 3000 in range start/stop
+    std::vector<int> ORFdefOverlap; // <- vector only for valid ones
+    int rescaleStart = fastaSeq.length() - 3000;
+    int rescaleStop = 3000;
+    for(size_t i = 0; i < ORFdefBoundary.size() / 2;i++){
+      if(ORFdefBoundary[2 * i] < 3000){ // start
+        if(ORFdefBoundary[2 * i + 1] > 3000){ // stop
+          ORFdefOverlap.push_back(ORFdefBoundary[2 * i] + rescaleStart);
+          ORFdefOverlap.push_back(ORFdefBoundary[2 * i + 1] - rescaleStop);
+        }
+      }
+    }
+    all_orfs.insert(all_orfs.end(), ORFdefOverlap.begin(), ORFdefOverlap.end());
+    Seqnames.insert(Seqnames.end(), ORFdefOverlap.size() / 2, n);
+    strands.insert(strands.end(),ORFdefOverlap.size() / 2,"+");
+
+    // now do on reverse compliment
+    std::reverse(fastaSeq.begin(),fastaSeq.end());
+    std::transform(fastaSeq.begin(), fastaSeq.end(), fastaSeq.begin(), complement);
+
+    ORFdef = orfs_as_vector(fastaSeq, startCodon,
+                                             stopCodon,longestORF,
+                                             minimumLength); // <- get all orfs for start to stop
+    all_orfs.insert(all_orfs.end(), ORFdef.begin(), ORFdef.end());
+    Seqnames.insert(Seqnames.end(), ORFdef.size() / 2, n);
+    strands.insert(strands.end(),ORFdef.size() / 2,"-");
+
+    //Now do stop/start boundary, +/-3000, only keep the ones who are overlapping start/stop boundary
+    startStopBoundary = fastaSeq.substr(fastaSeq.length()-3000,3000);
+    startStopBoundary.append(fastaSeq.substr(0,3000));
+    ORFdefBoundary = orfs_as_vector(startStopBoundary, startCodon,
+                                                     stopCodon,longestORF,
+                                                     minimumLength);
+
+    //now filter out wrong ones, the ones that does not contain point 3000 in range start/stop
+    std::vector<int> ORFdefOverlapMin; // <- vector only for valid ones
+
+    for(size_t i = 0; i < ORFdefBoundary.size() / 2;i++){
+      if(ORFdefBoundary[2 * i] < 3000){ // start
+        if(ORFdefBoundary[2 * i + 1] > 3000){ // stop
+          ORFdefOverlapMin.push_back(ORFdefBoundary[2 * i] + rescaleStart);
+          ORFdefOverlapMin.push_back(ORFdefBoundary[2 * i + 1] - rescaleStop);
+        }
+      }
+    }
+    all_orfs.insert(all_orfs.end(), ORFdefOverlapMin.begin(), ORFdefOverlapMin.end());
+    Seqnames.insert(Seqnames.end(), ORFdefOverlapMin.size() / 2, n);
+    strands.insert(strands.end(),ORFdefOverlapMin.size() / 2,"-");
+
+  }
+
+  // all_orfs is an interlaced vector. We de-interlace it into two vectors.
+  std::vector<vi> result_value(2);
+  result_value[0].resize(all_orfs.size() / 2);
+  result_value[1].resize(all_orfs.size() / 2);
+  for (size_t i = 0; i < all_orfs.size() / 2; i++) {
+    result_value[0][i] = all_orfs[2 * i];
+    result_value[1][i] = all_orfs[2 * i + 1];
+  }
+
+
+
+  return(GRangesC(wrap(Seqnames),
+                  IRangesC(wrap(result_value[0]),
+                    wrap(result_value[1])),wrap(strands)));
+}
+
+
